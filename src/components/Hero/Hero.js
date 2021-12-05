2c627be3c54bdfd4d7769dba6278279e9aeb2904
Doug Peterson <douglaspeterson6@gmail.com>
2021-12-05 14:33:11 -0600

    fixing images ' :

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -32,18 +32,15 @@ function Hero() {
                         </p>
                     </HeroLeft>
                     <HeroRight>
-                        <Image
-                            src="https://raw.githubusercontent.com/doug-coder64/website/main/src/Assets/scroll-down.svg"
-                            alt="image"
-                        />
+
                     </HeroRight>
                 </HeroWrapper>
                 <ScrollDown to="projects">
                     <ScrollLink>
                         Scroll Down
                         <img
-                            src=""
-                            alt=""
+                            src="../Assets/scroll-down.svg"
+                            alt="Scroll Down"
                         />
                     </ScrollLink>
                 </ScrollDown>
